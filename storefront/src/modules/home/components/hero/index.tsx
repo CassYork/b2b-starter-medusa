570e95ca0e507686991093dff4a7eabb69169b86
Jayson Tolentino <jaysontolentino.ph@gmail.com>
2024-12-04 18:52:18 +0800

    initial setup and remove banner

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -25,22 +25,13 @@ const Hero = () => {
             level="h1"
             className="text-6xl leading-10 text-ui-fg-base font-normal mt-10 mb-5"
           >
-            Portable Bestsellers
+            My Demo Store
           </Heading>
 
           <p className="leading-10 text-ui-fg-subtle font-normal text-lg">
             See our widest selection of electronics
           </p>
         </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary" className="rounded-2xl">
-            <Github />
-            Github Repository
-          </Button>
-        </a>
       </div>
     </div>
   )
